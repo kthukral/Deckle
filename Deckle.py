d017f9dbf5a928accb44ffb7abe70294586f0394
Erik Derohanian <erik.derohanian@gmail.com>
2013-03-28 18:16:26 -0400

    Initial commit
    	new file:   Deckle.glade
    	new file:   Deckle.py

---
+++ Deckle.py
@@ -0,0 +1,26 @@
+from gi.repository import Gtk
+
+class DeckleGui(Gtk.Window):
+    def __init__(self):
+        builder = Gtk.Builder()
+        builder.add_from_file("Deckle.glade")
+        builder.connect_signals(self)
+
+        self.window = builder.get_object("window1")
+        self.menubar = builder.get_object("menubar1")
+        self.toolbar = builder.get_object("toolbar1")
+        self.drawingArea = builder.get_object("drawingarea1")
+        self.statusbar = builder.get_object("statusbar1")
+
+        self.window.show_all()
+
+    def onDeleteWindow(self, *args):
+        Gtk.main_quit(*args)
+
+    def onFileNew(self, *args):
+        self.statusbar.push(0, "File>New")
+
+
+DeckleGui = DeckleGui()
+Gtk.main()
+
