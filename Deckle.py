8185f0d7baba6f08b47b52f45f5378f4c12a871e
Erik Derohanian <erik.derohanian@gmail.com>
2013-03-31 01:32:26 -0400

    Can draw with pen / mouse

--- Deckle.py
+++ Deckle.py
@@ -2,7 +2,12 @@ from gi.repository import Gtk
 from gi.repository import Gdk
 
 class DeckleGui(Gtk.Window):
+
+
     def __init__(self):
+        
+        self.clicks = []
+        
         builder = Gtk.Builder()
         builder.add_from_file("Deckle.glade")
         builder.connect_signals(self)
@@ -14,6 +19,7 @@ class DeckleGui(Gtk.Window):
         self.drawingArea = builder.get_object("drawingarea1")
         self.drawingArea.add_events(Gdk.EventMask.BUTTON1_MOTION_MASK)
         self.drawingArea.add_events(Gdk.EventMask.BUTTON_PRESS_MASK)
+        self.drawingArea.add_events(Gdk.EventMask.BUTTON_RELEASE_MASK)
         
         self.statusbar = builder.get_object("statusbar1")
 
@@ -27,19 +33,32 @@ class DeckleGui(Gtk.Window):
    
     def mainDraw(self, drawingArea, cr):
         print ("Drawing...")
+        cr.set_source_rgb(1,1,1)
+        cr.paint()
+        cr.set_source_rgb(0,0,0)
+        if len(self.clicks) > 1:
+            cr.move_to(self.clicks[0][0], self.clicks[0][1])
+            for point in self.clicks:
+                cr.line_to(point[0], point[1])
+            cr.stroke()
 
     def onDAButtonPress(self, widget, event):
         print ("Click at:  ", 
             int(event.x), ", ", 
-            int(event.y), ", state: ", 
-            event.state, sep="")
+            int(event.y), sep="")
+        return True
+
+    def onDAButtonRelease(self, widget, event):
+        print ("Gone!")
+
         return True
 
     def onDAMotion(self, widget, event):
         print ("Motion at: ", 
             int(event.x), ", ", 
-            int(event.y),", state: ", 
-            event.state, sep="")
+            int(event.y), sep="")
+        self.clicks.append( (int(event.x), int(event.y)) )
+        self.drawingArea.queue_draw()
 
 DeckleGui = DeckleGui()
 Gtk.main()
